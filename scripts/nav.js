8778a691c6fad3c409bf1746a51659baf7b84a08
Joe <joe.c.jacobson@gmail.com>
2019-06-07 11:53:09 -0600

    stuff

--- scripts/nav.js
+++ scripts/nav.js
@@ -2,7 +2,7 @@ Vue.component('nav-bar', {
     template: `<nav class="navbar navabar-expand-lg navbar-dark bg-dark">
                     <a class="navbar-brand" href="index.html">Home</a>
                     <a Class="navbar-brand" href='constuction.html'>Resumé</a>
-                    <a Class="navbar-brand" href='constuction.html'>Education</a>
+                    <a Class="navbar-brand" href='education.html'>Education</a>
                     <a Class="navbar-brand" href='constuction.html'>Projects</a>
                     <a Class="navbar-brand" href='contact.html'>Contact</a>
                </nav>`
